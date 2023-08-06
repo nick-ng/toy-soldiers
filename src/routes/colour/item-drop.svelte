b41bcb2cadf39e0890b3f275e5e92f83403d947f
Nick Ng <nick-ng@users.noreply.github.com>
2023-08-06 12:24:02 +1200

    feat: add colour picker for a different game

---
+++ src/routes/colour/item-drop.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let itemName = 'Rusted Sword';
+	export let borderHex = '#123456';
+	export let borderAlpha = 200;
+	export let backgroundHex = '#111111';
+	export let backgroundAlpha = 200;
+	export let textHex = '#ffffff';
+	export let textAlpha = 200;
+</script>
+
+<div
+	style={[
+		`border-color: ${borderHex}${borderAlpha.toString(16).padStart(2, '0')}`,
+		`color: ${textHex}${textAlpha.toString(16).padStart(2, '0')}`,
+		`background-color: ${backgroundHex}${backgroundAlpha.toString(16).padStart(2, '0')}`,
+		''
+	].join(';')}
+	class="p-2 border-solid border-[3px] inline-block font-semibold"
+>
+	{itemName}
+</div>
